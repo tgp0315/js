c05f1465be8acffd888ead0a29e0ebb3265c003f
v_tianguoping <v_tianguoping@100tal.com>
2020-11-19 22:46:00 +0800

    fix:优化bind

--- bind.js
+++ bind.js
@@ -2,6 +2,7 @@ const { type } = require("os");
 
 // 手写实现bind
 function myBind(context) {
+  context = context || window;
   if (typeof this !== "function") {
     return;
   }
@@ -9,7 +10,7 @@ function myBind(context) {
   var args = Array.prototype.slice(arguments, 1);
   var A = function() {};
   var B = function() {
-    var _this = this instanceof self ?  this : context;
+    var _this = this instanceof A ?  this : context;
     return self.apply(_this, args.concat(Array.prototype.slice.call(arguments, 0)));
   }
   if (this.prototype) {
